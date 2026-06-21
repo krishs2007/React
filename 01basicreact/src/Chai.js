9cc3412f96a05083328b6027d0eb436a7b88f246
Krish Sonani <krishsonani2007@gmail.com>
2026-06-21 20:56:08 +0530

    UPDATE FILE

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,7 @@
+function chai() {
+    return (
+        <h2>chai aur react</h2>
+    )
+}
+
+export default chai
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,11 @@
+import Chai from "./chai"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Chai aur react with Vite</h1>
+    <>
+      <Chai/>
+      <h1>chai aur react</h1>
+      <p>test para</p>
+    </>
   )
 }
 
