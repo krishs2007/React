3a3b29651e03df9997275e2829652bdb1b507e34
Krish Sonani <krishsonani2007@gmail.com>
2026-06-22 11:00:23 +0530

    UPDATE FILE

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,28 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// const reactElement = {
+//     type : 'a',
+//     props : {
+//         href : 'https://google.com',
+//         target: '_blank'
+//     },
+//     children : 'Click me to visit google'
+// }
+
+const anotherElement= (
+  <a href='https://google.com' target='_blank'>vist google</a>
+)
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target : '_blank'},
+    'click me to visit googl e'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    reactElement
 )
