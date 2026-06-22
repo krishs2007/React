bfcef6fc92096b1ae949d08b182e60c443c3c05d
Krish Sonani <krishsonani2007@gmail.com>
2026-06-22 18:20:53 +0530

    ADD FILE

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 15
+
+  const addValue = () => {
+    // console.log('clicked', counter);
+    // counter = counter + 1
+    if(counter<20) setCounter(counter+1)
+  }
+  const removeValue = () => {
+    if(counter>0) setCounter(counter-1)
+  }
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick = {addValue}>Add Value</button>
+      <br/>
+      <button onClick = {removeValue}>Remove Value</button>
+      <p>footer : {counter}</p>
+    </>
+  )
+}
+
+export default App
