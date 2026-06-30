0a107a46fd4901c111dd196d786d51f10623c034
Krish Sonani <krishsonani2007@gmail.com>
2026-06-30 18:41:49 +0530

    ADD FILE

--- 07reactRouter/src/components/Home/Home.jsx
+++ 07reactRouter/src/components/Home/Home.jsx
@@ -30,11 +30,11 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img className="w-96" src="https://images.pexels.com/photos/12024196/pexels-photo-12024196.jpeg" alt="image1" />
                 </div>
             </aside>
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img className="sm:w-96 w-48" src="https://images.pexels.com/photos/35640792/pexels-photo-35640792.jpeg" alt="image2" />
             </div>
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
         </div>
