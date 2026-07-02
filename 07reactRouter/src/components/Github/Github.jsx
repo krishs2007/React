a010f2d4ee01fcfe509f0d92d698021791771f05
Krish Sonani <krishsonani2007@gmail.com>
2026-07-02 17:08:26 +0530

    ADD FILE

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -10,7 +10,9 @@ function Github() {
         })
     }, [])
     return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}</div>
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+        <img src={data.avatar.url} className='text-center' alt="Git Picture" width={300}/>
+        </div>
     )
 }
 
