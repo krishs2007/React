929c19f23a8091d571d4f5aa433b536d1f59deb4
Krish Sonani <krishsonani2007@gmail.com>
2026-07-18 18:14:47 +0530

    ADD FILE

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -16,7 +16,7 @@ function Login() {
             <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} placeholder="username"/>
             <input type='text' value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password"/>
             <button onClick={handleSubmit}>Submit</button>
-            
+            //
         </div>
     )
 }
