d1e4002c9ec1542e58937f44be7605a0be5e4042
Krish Sonani <krishsonani2007@gmail.com>
2026-07-20 18:14:29 +0530

    ADD FILE

---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useContext} from 'react'
+import UserContext from  '../context/UserContext'
+
+function Login() {
+    const [username,setUsername] = useState('')
+    const [password, setPassword] = usestate('')
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type='text' placeholder='username'/>
+            <input type='text' placeholder='password'/>
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
