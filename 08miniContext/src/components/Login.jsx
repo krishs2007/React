866909548114e119e43d4afea40eac5c7a6e0501
Krish Sonani <krishsonani2007@gmail.com>
2026-07-20 20:03:29 +0530

    ADD FILE

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -4,11 +4,16 @@ import UserContext from  '../context/UserContext'
 function Login() {
     const [username,setUsername] = useState('')
     const [password, setPassword] = usestate('')
+    const {setUser} = useContext(UserContext)
+    const handleSubmit = () => {
+        e.preventDefault()
+        setUser({username,password})
+    }
     return (
         <div>
             <h2>Login</h2>
-            <input type='text' placeholder='username'/>
-            <input type='text' placeholder='password'/>
+            <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='username'/>
+            <input type='text' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password'/>
             <button onClick={handleSubmit}>Submit</button>
         </div>
     )
