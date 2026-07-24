bbe2d5db56f618e2d120f1d6e64d47a9e60413e5
Krish Sonani <krishsonani2007@gmail.com>
2026-07-24 23:54:39 +0530

    ADD FILE

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem('todos',JSON.stringify(todos))
-  })
+  }, [todos])
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleCompleted}}>
@@ -46,6 +46,11 @@ function App() {
             </div>
             <div className="flex flex-wrap gap-y-3">
                 {/*Loop and Add TodoItem here */}
+                  {todos.map((todo) => (
+                    <div key={todo.id} className='w-full'>
+                      <TodoItem todo={todo}/>
+                    </div>
+                  ))}
             </div>
         </div>
     </div>
