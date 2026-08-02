f5bd8da3b132e1e039cae1b48e2a5e0ecc9a6458
Krish Sonani <krishsonani2007@gmail.com>
2026-08-02 19:34:53 +0530

    ADD FILE

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,13 @@
+import { Client, Account, ID } from "appwrite";
+
+// const client = new Client()
+//     .setEndpoint('https://<REGION>.cloud.appwrite.io/v1') // Your API Endpoint
+//     .setProject('<PROJECT_ID>');                 // Your project ID
+
+// const account = new Account(client);
+
+// const user = await account.create({
+//     userId: ID.unique(), 
+//     email: 'email@example.com', 
+//     password: 'password'
+// });
