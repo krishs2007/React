22737e0d85d7c6c0ee17c15cfeb2547c42330c84
Krish Sonani <krishsonani2007@gmail.com>
2026-08-03 20:14:47 +0530

    ADD FILE

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -11,3 +11,38 @@ import { Client, Account, ID } from "appwrite";
 //     email: 'email@example.com', 
 //     password: 'password'
 // });
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appWriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email,password,name}) {
+        try {
+           const userAccount = await this.account.create(ID.unique(), email, password, name);
+           if(userAccount)
+           {
+                // call another Method
+            }
+            else return userAccount 
+        } catch(error) {
+            throw error;
+        }
+    }
+    async login({email, password})
+    {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default AuthService
