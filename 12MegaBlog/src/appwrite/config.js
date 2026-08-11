66eca1f631f92d4a29ab09693a9c7adec5548597
Krish Sonani <krishsonani2007@gmail.com>
2026-08-11 17:49:28 +0530

    ADD FILE

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -89,6 +89,39 @@ export class Service{
     }
 
     // file upload service
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.creareFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: uploadFiles :: error", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite Service :: deleteFiles :: error", error);
+            return false
+        }
+    }
+
+    getFilePreview (fileId) {
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 const service = new Service()
