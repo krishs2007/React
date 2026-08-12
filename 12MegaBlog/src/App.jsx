1404a8043dfd0f1a6cda1cc0d64396a38916f108
Krish Sonani <krishsonani2007@gmail.com>
2026-08-12 17:01:18 +0530

    ADD FILE

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -3,15 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import authSlice from './store/authSlice'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
 
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
   
-  return (
-    <>
-      <h1>sdmvaewrog asasowp</h1> //
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-sc'></div>
+  ) : null
 }
 
 export default App
