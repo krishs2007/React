84997e566b2a9076e1065d958f179088c05cf123
Krish Sonani <krishsonani2007@gmail.com>
2026-08-17 17:34:45 +0530

    ADD FILE

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -40,7 +40,7 @@ function Header() {
             <Container>
                 <nav className="flex">
                     <div className="mr-4">
-                        <Link to='/'>
+                        <Link to='/'> //
                             <Logo width='70px'/>
                         </Link>
                     </div>
