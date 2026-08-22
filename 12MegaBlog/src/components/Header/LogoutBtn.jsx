1a7d04f2ee6570c8817ddb221354460b245d7d41
Krish Sonani <krishsonani2007@gmail.com>
2026-08-22 20:27:08 +0530

    ADD FILE

--- 12MegaBlog/src/components/Header/LogoutBtn.jsx
+++ 12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -7,7 +7,7 @@ function LogoutBtn() {
     const dispatch = useDispatch()
     const logoutHandler = () => {
         authService.logout().then(() => {
-            dispatch(logout())
+            dispatch(logout()) //
         })
     }
     return (
