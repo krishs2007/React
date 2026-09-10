b9ab9d772feeaf15ced090788363473a3b0a644e
Krish Sonani <krishsonani2007@gmail.com>
2026-09-10 22:16:41 +0530

    ADD FILE

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -94,7 +94,7 @@ export class Service{
         try {
             return await this.bucket.createFile(
                 conf.appwriteBucketId,
-                ID.unique(),
+                ID.unique(), //
                 file
             )
         } catch (error) {
