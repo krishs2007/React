cdcfd644c560d97ac08824393562509df7160ccd
Krish Sonani <krishsonani2007@gmail.com>
2026-09-24 20:49:41 +0530

    UPDATE FILE

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -14,7 +14,7 @@ export class Service{
 
     async createPost({title, slug, content, featuredImage, status, userId}) {
         try {
-            return await this.databases.createDocument( 
+            return await this.databases.createDocument(  //
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug,
